6cd0ee09add9c019a1191f2af0c872034ef5288b
Arga Melvern <e203arga@gmail.com>
2023-08-10 01:44:21 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}">
+    <title>Document</title>
+</head> 
+<body>
+
+    <div class="container grid">
+        <div class="box">a</div>
+        <div class="box">b</div>
+        <div class="box">c</div>
+        <div class="box">d</div>
+    </div>
+    
+    <script src="{{ asset('js/script.js') }}"></script>
+
+</body>
+</html>
